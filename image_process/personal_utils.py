1f9accd532e14a603995ca0cc228b53f9f26f447
Coding Cat <80199661+With-Coding-Cat@users.noreply.github.com>
2021-03-18 16:27:04 +0900

    Update personal_utils.py

--- image_process/personal_utils.py
+++ image_process/personal_utils.py
@@ -3,9 +3,13 @@
 import cv2
 import os
 
+img_name_numbering = 0
 def take_picture(x, img, save_path):
+    global img_name_numbering
+    img_name_numbering += 1
     x1, y1, x2, y2 = int(x[0]), int(x[1]), int(x[2]), int(x[3])
-    roi = img[x1:x2,y1:y2].copy()
-    os.makedirs(save_path + '/img', exist_ok=True)
-    cv2.imwrite(save_path + '/img', roi)
+    roi = img[y1:y2,x1:x2].copy()
+    os.makedirs(str(save_path / 'img'), exist_ok=True)
+    #cv2.imwrite(str(save_path / 'img/{}.jpg'.format(time.localtime(time.time()))), roi)
+    cv2.imwrite(str(save_path / 'img/{}.jpg'.format(img_name_numbering)), roi)
     return roi
