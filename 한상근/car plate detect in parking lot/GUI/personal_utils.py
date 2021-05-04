84b70a825db49d480bd60e3a63c29f11df12849b
Coding Cat <80199661+With-Coding-Cat@users.noreply.github.com>
2021-05-04 13:32:39 +0900

    Update personal_utils.py

--- 한상근/car plate detect in parking lot/GUI/personal_utils.py
+++ 한상근/car plate detect in parking lot/GUI/personal_utils.py
@@ -6,6 +6,8 @@ from OCR.utils import CTCLabelConverter, AttnLabelConverter
 import torch.nn.functional as F
 import torch
 
+import numpy as np
+
 #img_name_numbering = 0
 def take_picture(x, img):
     #global img_name_numbering
@@ -17,7 +19,298 @@ def take_picture(x, img):
     #cv2.imwrite(str(save_path / 'img/{}.jpg'.format(img_name_numbering)), roi)
     return roi
 
+def predict_plate(model, opt, image_to_give, device, converter):
+    MAX_DIAG_MULTIPLYER = 15 # 5
+    MAX_ANGLE_DIFF = 20.0 # 12.0
+    MAX_AREA_DIFF = 0.5 # 0.5
+    MAX_WIDTH_DIFF = 0.8
+    MAX_HEIGHT_DIFF = 0.2
+    MIN_N_MATCHED = 4 # 3
+    MIN_PLATE_RATIO = 3
+    MAX_PLATE_RATIO = 10
+    def find_chars(contour_list):
+        matched_result_idx = []
+        
+        for d1 in contour_list:
+            matched_contours_idx = []
+            for d2 in contour_list:
+                if d1['idx'] == d2['idx']:
+                    continue
 
+                dx = abs(d1['cx'] - d2['cx'])
+                dy = abs(d1['cy'] - d2['cy'])
+
+                diagonal_length1 = np.sqrt(d1['w'] ** 2 + d1['h'] ** 2)
+
+                distance = np.linalg.norm(np.array([d1['cx'], d1['cy']]) - np.array([d2['cx'], d2['cy']]))
+                if dx == 0:
+                    angle_diff = 90
+                else:
+                    angle_diff = np.degrees(np.arctan(dy / dx))
+                area_diff = abs(d1['w'] * d1['h'] - d2['w'] * d2['h']) / (d1['w'] * d1['h'])
+                width_diff = abs(d1['w'] - d2['w']) / d1['w']
+                height_diff = abs(d1['h'] - d2['h']) / d1['h']
+
+                if distance < diagonal_length1 * MAX_DIAG_MULTIPLYER \
+                and angle_diff < MAX_ANGLE_DIFF and area_diff < MAX_AREA_DIFF \
+                and width_diff < MAX_WIDTH_DIFF and height_diff < MAX_HEIGHT_DIFF:
+                    matched_contours_idx.append(d2['idx'])
+
+            # append this contour
+            matched_contours_idx.append(d1['idx'])
+
+            if len(matched_contours_idx) < MIN_N_MATCHED:
+                continue
+
+            matched_result_idx.append(matched_contours_idx)
+
+            unmatched_contour_idx = []
+            for d4 in contour_list:
+                if d4['idx'] not in matched_contours_idx:
+                    unmatched_contour_idx.append(d4['idx'])
+
+            unmatched_contour = np.take(possible_contours, unmatched_contour_idx)
+            
+            # recursive
+            recursive_contour_list = find_chars(unmatched_contour)
+            
+            for idx in recursive_contour_list:
+                matched_result_idx.append(idx)
+
+            break
+
+        return matched_result_idx
+
+    img_ori = cv2.resize(image_to_give, dsize=(200, 70), interpolation=cv2.INTER_CUBIC)
+
+    height, width, channel = img_ori.shape
+
+    gray = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, ksize=(5, 5), sigmaX=0)
+
+    sharpening_1 = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    dst = cv2.filter2D(blur, -1, sharpening_1)
+
+    thresh = cv2.adaptiveThreshold(dst, 
+        maxValue=255.0, 
+        adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+        thresholdType=cv2.THRESH_BINARY_INV, 
+        blockSize=39, 
+        C=9)
+    rect_kern = cv2.getStructuringElement(cv2.MORPH_CROSS, (2,2))
+    # apply dilation to make regions more clea
+    dilation = cv2.dilate(thresh, rect_kern, iterations = 1)
+    contours, _ = cv2.findContours(
+        dilation, 
+        mode=cv2.RETR_LIST, 
+        method=cv2.CHAIN_APPROX_SIMPLE
+    )
+    height = height
+    width = width
+    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+    cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
+
+    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+    contours_dict = []
+
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(temp_result, pt1=(x, y), pt2=(x+w, y+h), color=(255, 255, 255), thickness=2)
+
+        # insert to dict
+        contours_dict.append({
+            'contour': contour,
+            'x': x,
+            'y': y,
+            'w': w,
+            'h': h,
+            'cx': x + (w / 2),
+            'cy': y + (h / 2)
+        })
+
+    possible_contours = []
+
+    cnt = 0
+    for d in contours_dict:
+        area = d['w'] * d['h']
+        ratio = d['w'] / d['h']
+
+        if area > 200 \
+        and d['w'] > 10 and d['h'] > 15 :
+            d['idx'] = cnt
+            cnt += 1
+            possible_contours.append(d)
+
+    # visualize possible contours
+    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+    for d in possible_contours:
+    #     cv2.drawContours(temp_result, d['contour'], -1, (255, 255, 255))
+        cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x']+d['w'], d['y']+d['h']), color=(255, 255, 255), thickness=2)
+
+
+
+
+    result_idx = find_chars(possible_contours)
+
+    matched_result = []
+    for idx_list in result_idx:
+        matched_result.append(np.take(possible_contours, idx_list))
+
+    # visualize possible contours
+    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+    for r in matched_result:
+        for d in r:
+    #         cv2.drawContours(temp_result, d['contour'], -1, (255, 255, 255))
+            cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x']+d['w'], d['y']+d['h']), color=(255, 255, 255), thickness=2)
+
+
+    # rotate plate images
+    MIN_PLATE_RATIO = 3
+    MAX_PLATE_RATIO = 12
+    PLATE_WIDTH_PADDING = 4.0 # 1.3
+    PLATE_HEIGHT_PADDING = 1.5 # 1.5
+
+    plate_imgs = []
+    plate_infos = []
+
+    for i, matched_chars in enumerate(matched_result):
+        sorted_chars = sorted(matched_chars, key=lambda x: x['cx'])
+
+        plate_cx = (sorted_chars[0]['cx'] + sorted_chars[-1]['cx']) / 2
+        plate_cy = (sorted_chars[0]['cy'] + sorted_chars[-1]['cy']) / 2
+
+        plate_width = (sorted_chars[-1]['x'] + sorted_chars[-1]['w'] - sorted_chars[0]['x']) * PLATE_WIDTH_PADDING
+
+        sum_height = 0
+        for d in sorted_chars:
+            sum_height += d['h']
+
+        plate_height = int(sum_height / len(sorted_chars) * PLATE_HEIGHT_PADDING)
+
+        triangle_height = sorted_chars[-1]['cy'] - sorted_chars[0]['cy']
+        triangle_hypotenus = np.linalg.norm(
+            np.array([sorted_chars[0]['cx'], sorted_chars[0
+                                                         ]['cy']]) - 
+            np.array([sorted_chars[-1]['cx'], sorted_chars[-1]['cy']])
+        )
+
+        angle = np.degrees(np.arcsin(triangle_height / triangle_hypotenus))
+
+        rotation_matrix = cv2.getRotationMatrix2D(center=(plate_cx, plate_cy), angle=angle, scale=1.0)
+
+        img_rotated = cv2.warpAffine(dilation, M=rotation_matrix, dsize=(width, height))
+        rect_kern = cv2.getStructuringElement(cv2.MORPH_CROSS, (1,1))
+        # apply dilation to make regions more clea
+        img_dilation = cv2.dilate(img_rotated, rect_kern, iterations = 1)
+        img_cropped = cv2.getRectSubPix(
+            img_dilation, 
+            patchSize=(int(plate_width), int(plate_height)), 
+            center=(int(plate_cx), int(plate_cy))
+        )
+
+        if img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO or img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO > MAX_PLATE_RATIO:
+            continue
+
+        plate_imgs.append(img_cropped)
+        plate_infos.append({
+            'x': int(plate_cx - plate_width / 2),
+            'y': int(plate_cy - plate_height / 2),
+            'w': int(plate_width),
+            'h': int(plate_height)
+        })
+    MIN_AREA = 80
+    MIN_WIDTH, MIN_HEIGHT = 3, 15
+    MIN_RATIO, MAX_RATIO = 0.2, 2.0
+
+
+    for i, plate_img in enumerate(plate_imgs):
+        plate_img = cv2.resize(plate_img, dsize=(0, 0), fx=1.6, fy=1.6)
+        _, plate_img = cv2.threshold(plate_img, thresh=0.0, maxval=255.0, type=cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+        # find contours again (same as above)
+        contours, _ = cv2.findContours(plate_img, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
+
+        plate_min_x, plate_min_y = plate_img.shape[1], plate_img.shape[0]
+        plate_max_x, plate_max_y = 0, 0
+
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+
+            area = w * h
+            ratio = w / h
+
+            if area > MIN_AREA \
+            and w > MIN_WIDTH and h > MIN_HEIGHT \
+            and MIN_RATIO < ratio < MAX_RATIO:
+                if x < plate_min_x:
+                    plate_min_x = x
+                if y < plate_min_y:
+                    plate_min_y = y
+                if x + w > plate_max_x:
+                    plate_max_x = x + w
+                if y + h > plate_max_y:
+                    plate_max_y = y + h
+
+        img_result = plate_img[plate_min_y:plate_max_y, plate_min_x:plate_max_x]
+
+        _, img_result = cv2.threshold(img_result, thresh=0.0, maxval=255.0, type=cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        img_result = cv2.copyMakeBorder(img_result, top=10, bottom=10, left=10, right=10, borderType=cv2.BORDER_CONSTANT, value=(0,0,0))
+        image_to_give = Image.fromarray(img_result)
+
+    # image_to_give = cv2.cvtColor(image_to_give, cv2.COLOR_BGR2RGB)
+    # image_to_give = cv2.cvtColor(image_to_give, cv2.COLOR_BGR2GRAY)
+    # image_to_give = Image.fromarray(image_to_give)
+
+    #print(image_to_give)
+    #print(type(image_to_give))
+    #print(image_to_give.shape)
+
+
+    AlignCollate_demo = AlignCollate()
+    demo_data = personal_dataset(image=image_to_give, opt=opt)
+
+
+    demo_loader = torch.utils.data.DataLoader(
+        demo_data, batch_size=1,
+        shuffle=False,
+        num_workers=int(0),
+        collate_fn=AlignCollate_demo, pin_memory=True)
+
+    # predict
+    #model.eval()
+    try:
+        with torch.no_grad():
+            for image_tensors, _ in demo_loader:
+                batch_size = image_tensors.size(0)
+                image = image_tensors.to(device)
+                # For max length prediction
+                length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(device)
+                text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(device)
+                preds = model(image, text_for_pred, is_train=False)
+                # select max probabilty (greedy decoding) then decode index to character
+                _, preds_index = preds.max(2)
+                preds_str = converter.decode(preds_index, length_for_pred)
+
+
+                preds_prob = F.softmax(preds, dim=2)
+                preds_max_prob, _ = preds_prob.max(dim=2)
+                for pred in preds_str:
+                    if 'Attn' in opt.Prediction:
+                        pred_EOS = pred.find('[s]')
+                        pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+ 
+
+
+                #출력하기
+                return pred
+    except:
+        return "None"
+
+'''
 def predict_plate(model, opt, image_to_give, device, converter):
 
     image_to_give = cv2.cvtColor(image_to_give, cv2.COLOR_BGR2RGB)
@@ -60,3 +353,4 @@ def predict_plate(model, opt, image_to_give, device, converter):
 
             #출력하기
             return pred
+'''
